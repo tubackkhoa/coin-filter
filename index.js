42291bdbc0c95356732600f0d12d34785ca79cc6
tuorai <tubackkhoa@gmail.com>
2021-08-26 10:35:49 +0700

    add tor proxy

--- index.js
+++ index.js
@@ -2,28 +2,63 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const puppeteer = require('puppeteer');
-const data = { Name: 'Oraichain Token' };
-let running = true;
 
+let page = null;
 if (process.env.NODE_ENV === 'production') console.log = () => {};
 
-const delay = (time) =>
-  new Promise(function (resolve) {
-    setTimeout(resolve, time);
-  });
+app.get('/:name', (req, res) => {
+  const { name } = req.params;
+  const data = {
+    name: name
+      .replace(/-+/g, ' ')
+      .replace(/(?<=^|\s)./g, (m) => m.toUpperCase())
+  };
+  await page.goto(`https://www.coingecko.com/en/coins/${name}`);
 
-app.get('/', (req, res) => {
-  res.json(data);
+  const start = Date.now();
+
+  try {
+    data.CurrentPrice = await(
+      await page.waitForSelector('span.tw-text-3xl')
+    ).evaluate((el) => el.innerText.trim());
+
+    Object.assign(
+      data,
+      Object.fromEntries(
+        await page.$$eval('div.order-3 tr', (list) =>
+          list.map((tr) => [
+            tr.querySelector('th').innerText.trim().replace(/[ /-]/g, ''),
+            tr.querySelector('td').innerText.trim()
+          ])
+        )
+      )
+    );
+
+    Object.assign(
+      data,
+      Object.fromEntries(
+        await page.$$eval('div.tw-flex-grow', (list) =>
+          list.slice(0, 6).map((tr) => {
+            const [td1, td2] = tr.innerText.trim().split(/\s(?=\$?\d)/);
+            return [td1.replace(/[ /-]/g, ''), td2];
+          })
+        )
+      )
+    );
+
+    console.log('take', Date.now() - start, 'ms');
+    res.json(data);
+  } catch (ex) {
+    res.end(ex.message);
+  }
 });
-
-app.use(express.static('public'));
 
 app.listen(port, async () => {
   console.log(`App listening at port:${port}`);
-
-  const browser = await puppeteer.launch();
+  const args = process.env.PROXY ? [`--proxy-server=${process.env.PROXY}`] : [];
+  const browser = await puppeteer.launch({ args });
   const page = await browser.newPage();
-  await page.setRequestInterception(true);
+  page.setRequestInterception(true);
   // ignore images,css,font
   page.on('request', (request) => {
     if (
@@ -34,51 +69,8 @@ app.listen(port, async () => {
       request.continue();
     }
   });
-  await page.goto('https://www.coingecko.com/en/coins/oraichain-token');
-
-  while (running) {
-    const start = Date.now();
-
-    try {
-      data.CurrentPrice = await (
-        await page.waitForSelector('span.tw-text-3xl')
-      ).evaluate((el) => el.innerText.trim());
-
-      Object.assign(
-        data,
-        Object.fromEntries(
-          await page.$$eval('div.order-3 tr', (list) =>
-            list.map((tr) => [
-              tr.querySelector('th').innerText.trim().replace(/[ /-]/g, ''),
-              tr.querySelector('td').innerText.trim()
-            ])
-          )
-        )
-      );
-
-      Object.assign(
-        data,
-        Object.fromEntries(
-          await page.$$eval('div.tw-flex-grow', (list) =>
-            list.slice(0, 6).map((tr) => {
-              const [td1, td2] = tr.innerText.trim().split(/\s(?=\$?\d)/);
-              return [td1.replace(/[ /-]/g, ''), td2];
-            })
-          )
-        )
-      );
-
-      console.log(data, 'take', Date.now() - start, 'ms');
-      await delay(5000);
-      await page.reload({ waitUntil: ['networkidle0', 'domcontentloaded'] });
-    } catch (ex) {
-      console.log(ex.message);
-    }
-  }
-
-  await browser.close();
 });
 
-process.on('SIGINT', function () {
-  running = false;
+process.on('SIGINT', async () => {
+  await browser.close();
 });
